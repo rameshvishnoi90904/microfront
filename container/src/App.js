694c8622798047a28b1949841a760177eedb761c
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2022-09-07 16:57:55 +0530

    first commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from 'bookingModule/Counter';
+function App() {
+  return (
+    <div className="App">
+      <h3>container </h3>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
